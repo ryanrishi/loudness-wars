f9b8609f3823d2a2474a1c1b544920594521fc16
Ryan Rishi <ryan@jelli.com>
2018-11-24 17:25:29 -0800

    add method to format results for CSV

--- src/writer.py
+++ src/writer.py
@@ -3,10 +3,22 @@ import csv
 PATH_PREFIX = 'out/'
 
 
-def write_results_to_file(filename, results):
-    """
+def format_row_for_remaster_csv(track_info, loudness, is_remaster=False):
+    # loudness comes from audio_analysis, everything else if from track
+    # song, artist, release_date (OG/remaster), remaster (y/n), loudness, track_id
+    row = {
+        "song": track_info["name"],
+        "artist": ', '.join(artist["name"] for artist in track_info["artists"]),
+        "release_date": track_info["album"]["release_date"],
+        "is_remaster": is_remaster,
+        "loudness": loudness,
+        "track_id": track_info["id"]
+    }
     
-    """
+    return row
+
+
+def write_results_to_file(filename, results):
     filename = "%s%s" % (PATH_PREFIX, filename)
     with open(filename, "w+") as f:
         fieldnames = ['Position', 'Song Title', 'Artist', 'Loudness']
